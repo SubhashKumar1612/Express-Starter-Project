879c7412ccfe864e0fcf3ff50e222b55528e768b
subhash <root@gmail.com>
2024-06-28 10:25:55 +0530

    connected to mongodb server

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,6 +2,9 @@
 const dotenv=require("dotenv")
 dotenv.config();
 
+
+// here we are exporting all the env valriable
 module.exports={
-    PORT:process.env.PORT
+    PORT:process.env.PORT,
+    DB_URL:process.env.DB_URL
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express=require("express")
 
 const serverConfig=require('./config/serverConfig')
+const connectDB=require("./config/dbConfig")
 const app=express();
 
-app.listen(serverConfig.PORT,()=>{
+app.listen(serverConfig.PORT,async()=>{
+    await connectDB();
     console.log(`server started at ${serverConfig.PORT}......`)
 })
+//ZqZbLRwZEoQMv3Pn   subhashkumarr1612
