e49eb9d77e24343f63d5607fdb48080638ce412e
subhash <root@gmail.com>
2024-06-28 07:52:05 +0530

    initial code

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express=require("express")
+
+const serverConfig=require('./config/serverConfig')
+const app=express();
+
+app.listen(serverConfig.PORT,()=>{
+    console.log(`server started at ${serverConfig.PORT}......`)
+})
